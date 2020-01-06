f74125b24f44b0cbd8d19f72792ca375a15204a9
alinapo1997 <alinapo1997@gmail.com>
2020-01-06 22:26:00 +0400

    Hello world app

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
